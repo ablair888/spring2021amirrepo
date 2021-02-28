b32d30f04d32da726639b6479ab4f49b084b3978
blaira1 <77460845+blaira1@users.noreply.github.com>
2021-02-28 13:29:36 -0500

    Add files via upload

--- Module7Script.R
+++ Module7Script.R
@@ -19,5 +19,4 @@ str(airmiles)
 str(beaver1)
 
 #main difference between S3 and S4 is that S4 is more formal than S3
-#Becasue S4 are more formal, they allow for specific callings of different (and multiple) inheritances and dispatch. 
-#RC objects are mutable, while methods belong to classes, not functions.
+#Because S4 are more formal, they allow for specific callings of different (and multiple) inheritances and dispatch. 
